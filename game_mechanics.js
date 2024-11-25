305682a20a934d71a8b42a662a84a2af71cba56b
VedangC-JU <chve24wm@student.ju.se>
2024-11-25 17:52:05 +0100

    Veggie Spawning and state transition
    Added vegetable spawning and game state transitions.

--- game_mechanics.js
+++ game_mechanics.js
@@ -6,9 +6,54 @@ function setup() {
     background(255, 140, 0);
     game.update();
     game.display();
-      }
- 
+  }
 
+class VeggieWarrior {
+    constructor() {
+      this.state = "start";                   // start, game, howToPlay, end
+      this.veggies = [];                      // Array to store vegetable objects
+      this.maxVeggies = 1;                    // Number of veggies to spawn initially
+      this.veggieSpawnTimer = 0;              // Timer for spawning veggies
+      this.lives = 3;                         // Player starts with 3 lives
+      this.score = 0;                         // Initial score
+  
+      // Spawn the first veggie
+      this.spawnVeggie();
+    }
+  
+    spawnVeggie() {
+      // Add a new vegetable object to the array
+      this.veggies.push(new Vegetable(random(width), height - 20, random(30, 50)));
+    }
+
+    update() {
+        if (this.state === "game") {
+          // Update veggies' positions
+          for (let i = this.veggies.length - 1; i >= 0; i--) {
+            let veg = this.veggies[i];
+            veg.update();
+    
+            // If a veggie falls off screen, reduce a life
+            if (veg.isOffScreen()) {
+              this.veggies.splice(i, 1);
+              this.lives--;
+            }
+          }
+    
+          // Spawn new veggies based on timer
+          this.veggieSpawnTimer++;
+          if (this.veggieSpawnTimer > 100) {
+            this.spawnVeggie();
+            this.veggieSpawnTimer = 0;
+          }
+    
+          // Check for game over
+          if (this.lives <= 0) {
+            this.state = "end";
+          }
+        }
+      }
+}
 // Vegetable Class
 class Vegetable {
     constructor(x, y, size) {
